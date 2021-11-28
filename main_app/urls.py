16d5bcc9093661ff7e6c370f768ea5028545ec89
fattony26 <marco.moran26@hotmail.com>
2021-11-28 17:31:30 -0500

    made migrations

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('pokemons/', views.pokemons_index, name='pokemons_index'),
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('pokemons/', views.pokemons_index, name='pokemons_index'),
+  path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='pokemons_detail'),
+  path('pokemons/create', views.PokemonCreate.as_view(), name='pokemons_create'),
+  path('pokemons/<int:pk>/update/', views.PokemonUpdate.as_view(), name='pokemons_update'),
+  path('pokemons/<int:pk>/delete/', views.PokemonDelete.as_view(), name='pokemons_delete'),
+  path('pokemons/<int:pokemon_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('pokemons/<int:pokemon_id_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('pokemons/<int:pokemon_id_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+  path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+  path('toys/', views.ToyList.as_view(), name='toys_index'),
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,65 @@
-from django.shortcuts import render
-# from .models import Pokemon
+from django.shortcuts import render, redirect
+from .models import Pokemon, Toy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+from django.views.generic import ListView, DetailView
 
-class Pokemon: 
-  def __init__(self, name, hp, description, age):
-    self.name = name
-    self.hp = hp
-    self.description = description
-    self.age = age
+# Create your views here.
 
-pokemons = [
-  Pokemon('Pikachu', '9000', 'Abrasive to strangers, and loyal to friends.', 3),
-  Pokemon('Squirtle', '150', 'Looks like a turtle.', 0),
-  Pokemon('Bulbasaur', '100', 'Loves the jazz cabbage.', 4),
-  Pokemon('Charmander', '300', 'Just wants to see the world burn.', 6)
-]
+class PokemonCreate(CreateView):
+  model = Pokemon
+  fields = ['name', 'hp', 'description', 'age']
+  success_url = '/pokemons/'
+class PokemonUpdate(UpdateView):
+  model = Pokemon
+  fields = ['hp', 'description', 'age']
+class PokemonDelete(DeleteView):
+  model = Pokemon
+  success_url = '/pokemons/'
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+class ToyList(ListView):
+  model = Toy
+class ToyDetail(DetailView):
+  model = Toy
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
+
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
+
 def pokemons_index(request):
-    pokemons = Pokemon.objects.all()
-    return render(request, 'pokemons/index.html', {'pokemons': pokemons })
+  pokemons = Pokemon.objects.all()
+  return render(request, 'pokemons/index.html', {
+    'pokemons': pokemons
+  })
+
+def pokemons_detail(request, pokemon_id):
+  pokemon = Pokemon.objects.get(id=pokemon_id)
+  toys_pokemon_doesnt_have = Toy.objects.exclude(id__in = pokemon.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'pokemons/detail.html', {
+    'pokemon': pokemon, 
+    'feeding_form': feeding_form, 
+    'toys': toys_pokemon_doesnt_have
+  })
+
+def add_feeding(request, pokemon_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.pokemon_id = pokemon_id
+    new_feeding.save()
+  return redirect('pokemons_detail', pokemon_id=pokemon_id)
+
+def assoc_toy(request, pokemon_id, toy_id):
+  Pokemon.objects.get(id=pokemon_id).toys.add(toy_id)
+  return redirect('pokemons_detail', pokemon_id=pokemon_id)
