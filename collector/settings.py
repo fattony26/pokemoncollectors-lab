e07d85d0ef269e2e2259618992e00b421a098519
fattony26 <marco.moran26@hotmail.com>
2021-11-29 09:01:29 -0500

    fixed secret key

--- collector/settings.py
+++ collector/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 from pathlib import Path
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1#wdz^$y-^1)8gp0=2r84vvnldmn=gk!4^k@%e%morux9-k)0v'
+SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
